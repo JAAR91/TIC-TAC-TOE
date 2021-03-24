c4e3f808203d3767f1cd8e0ab8c9ad057d1afdd2
errea <erreakezy@gmail.com>
2021-03-24 17:06:37 +0100

    Add prompts for welcome and playersnames

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,15 @@
 #!/usr/bin/env ruby
 
-puts 'Hello world!'
+puts "Welcome to Tic-Tac-Toe game, play fair and win"
+
+print "Enter your name Player one"
+first_player = gets.chomp
+first_player.capitalize!
+
+print "Enter your name Player Two"
+second_player = gets.chomp
+second_player.capitalize!
+
+puts "Let the game begin #{first_player} is X  and #{second_player} is O "
+
+
