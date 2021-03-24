3bfc7cc541955fb95d4e60afc6125ad26166d357
JAAR96 <jaarriagaramos@gmail.com>
2021-03-24 14:18:00 -0600

    remove code that cleans the screeb due to linter erros and incompatibilty

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,79 @@
-#!/usr/bin/env ruby
-require 'rbconfig'
-include RbConfig
+# !/usr/bin/env ruby
 
-def print_game(array)
+def start_screen
+  m_asw = 0
+  while m_asw != 4
+    jaarix('Welcome to Tic-Tac-Toe game, play fair and win')
+    print_game
+    m_asw = menu
+    case m_asw
+    when 1
+      game_start
+    when 2
+      game_instructions
+    when 3
+      game_credits
+    when 4
+      jaarix('Good Bye!')
+    end
+  end
+end
+
+def menu
+  # main menu, this let you choose wich option of the game you want to see
+  puts '--->Menu(Enter a number)'
+  puts '1.- Start Game'
+  puts '2.- Instructions'
+  puts '3.- Credits'
+  puts '4.- Exit'
+  gets.chomp.to_i
+end
+
+def game_start
+  jaarix('Please enter your name Player one?:')
+  first_player = gets.chomp
+  first_player.capitalize!
+
+  second_player = ''
+  game_info(first_player, second_player)
+  jaarix('Please enter your name Player Two?:')
+  second_player = gets.chomp
+  second_player.capitalize!
+
+  game_info(first_player, second_player)
+  puts 'Let the game begin!!!!'
+  jaarix("#{first_player} will be using the X  and #{second_player} will be using the O !!")
+  puts 'Press enter to continue'
+
+  game_info(first_player, second_player)
+
+  print_game
+  puts 'Please select an available spot from the board'
+  # here we will assing aswer to a variable
+
+  # here comes the game logic
+end
+
+def game_instructions
+  # game instrucctions will show up here
+  jaarix('Game instructions are under development please wait....')
+  puts 'Press enter to go back to the menu'
+end
+
+def game_credits
+  # game credits will show up here
+  jaarix('Game Credits are under development please wait....')
+  puts 'Press enter to go back to the menu'
+end
+
+def print_game
+  array = [['+---+---+---+'],
+           ['| ', 1, ' | ', 2, ' | ', 3, ' |'],
+           ['+---+---+---+'],
+           ['| ', 4, ' | ', 5, ' | ', 6, ' |'],
+           ['+---+---+---+'],
+           ['| ', 7, ' | ', 8, ' | ', 9, ' |'],
+           ['+---+---+---+']]
   puts ''
   puts array[0]
   array[1].each { |item| print item }
@@ -17,20 +88,19 @@ def print_game(array)
   puts ''
 end
 
-def game_info(p1, p2)
+def game_info(player1, player2)
   m_line = []
-  n_size = 0
-  n_size = if p1.length > p2.length
-             p1.length
+  n_size = if player1.length > player2.length
+             player1.length
            else
-             p2.length
+             player2.length
            end
   (n_size + 19).times { |_i| m_line.push('-') }
   print '+'
   m_line.each { |item| print item }
   puts '+'
-  puts "|Player number one: #{p1}"
-  puts "|Player number two: #{p2}"
+  puts "|Player number one: #{player1}"
+  puts "|Player number two: #{player2}"
   print '+'
   m_line.each { |item| print item }
   puts '+'
@@ -56,80 +126,4 @@ def jaarix(input)
   puts array[3]
 end
 
-def screenclear
-  case CONFIG['host_os']
-  when /mswin|windows/i
-    system('cls')
-  when /linux|arch/i
-    system('clear')
-  when /darwin/i
-    system('clear')
-  end
-end
-
-gamearray = [['+---+---+---+'], ['| ', 1, ' | ', 2, ' | ', 3, ' |'], ['+---+---+---+'], ['| ', 4, ' | ', 5, ' | ', 6, ' |'], ['+---+---+---+'], ['| ', 7, ' | ', 8, ' | ', 9, ' |'], ['+---+---+---+']]
-first_player = ''
-second_player = ''
-m_asw = 0
-while m_asw != 4
-  screenclear
-  jaarix('Welcome to Tic-Tac-Toe game, play fair and win')
-  print_game(gamearray)
-
-  # main menu, this let you choose wich option of the game you want to see
-
-  puts '--->Menu(Enter a number)'
-  puts '1.- Start Game'
-  puts '2.- Instructions'
-  puts '3.- Credits'
-  puts '4.- Exit'
-  m_asw = gets.chomp.to_i
-
-  case m_asw
-  when 1
-    screenclear
-    jaarix('Please enter your name Player one?:')
-    first_player = gets.chomp
-    first_player.capitalize!
-
-    screenclear
-    game_info(first_player, second_player)
-    jaarix('Please enter your name Player Two?:')
-    second_player = gets.chomp
-    second_player.capitalize!
-
-    screenclear
-    game_info(first_player, second_player)
-    puts 'Let the game begin!!!!'
-    jaarix("#{first_player} will be using the X  and #{second_player} will be using the O !!")
-    puts 'Press enter to continue'
-    p_enter = gets
-
-    screenclear
-    game_info(first_player, second_player)
-    game_asw = 0
-
-    print_game(gamearray)
-    puts 'Please select an available spot from the board'
-    game_asw = gets.chomp.to_i
-
-    p_enter = gets
-    # here comes the game logic
-
-  when 2
-    # game instrucctions will show up here
-    screenclear
-    jaarix('Game instructions are under development please wait....')
-    puts 'Press enter to go back to the menu'
-    p_enter = gets
-  when 3
-    # game credits will show up here
-    screenclear
-    jaarix('Game Credits are under development please wait....')
-    puts 'Press enter to go back to the menu'
-    p_enter = gets
-  when 4
-    screenclear
-    jaarix('Good Bye!')
-  end
-end
+start_screen
