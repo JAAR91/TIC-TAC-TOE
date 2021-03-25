67f342c4e17f769bd21d10f7b45749bd71b573e1
JAAR96 <jaarriagaramos@gmail.com>
2021-03-25 13:16:52 -0600

    add credits.rb and add credits information on it

---
+++ bin/credits.rb
@@ -0,0 +1,28 @@
+class Credits
+    def display_credits
+        display = Display.new
+        display.display_clear
+        puts '+----------------------------------------------------------------------------------+'
+        puts '|   lllllllllllllll       ]llllllllllllllL           }lllllllllllllj               |'
+        puts '|   lllll||||||llll       Jllll|l||||||llL           lllll||||l|llll               |'
+        puts '|        ||||l  www  ,wyww,    l||||L  wwwwww   ,wwww,    L||||  ,wwww,  ;wwwww,   |'
+        puts '|        l|||l  lll ||||jl|j   ll|||L  ||l||l  l|lrl||L   ||||l jlll*|ll ||||***   |'
+        puts '|        |||||  l|l ||||       ll|||L ;||ll||L ||lL       L|||l ]||l ||l ||||ll    |'
+        puts '|        |||||  l|l ||||J$$%   l||||L $|ll$||l l|lL$$$L   L|||l ]||l,||l ||||,,,   |'
+        puts '|        YWWWW  WWW  "4&l&*`   YWWWWL WWW""WW&  *&&lW*    YWWWW  lY&l&M  !WWWWWF   |'
+        puts '+----------------------------------------------------------------------------------+'
+        puts '|       Thank you for playing the game!                                            |'
+        puts '|            v                                                                     |'
+        puts '|                                                                                  |'
+        puts '|          (°v°)                                           BY:                     |'
+        puts '|         /|-=-|\                                          JAAR                    |'
+        puts '|         ( _=_ )                                          ERI                     |'
+        puts '|          ^^ ^^                                                                   |'
+        puts '|                                                                      MICROVERSE  |'
+        puts '+----------------------------------------------------------------------------------+'
+        puts 'Press any key to go back to menu....'
+        gets
+        menu = Menu.new
+        menu.game_menu
+    end
+end
--- bin/gamerules.rb
+++ bin/gamerules.rb
@@ -9,7 +9,7 @@ class Gamerules
   def show_game_rules
     @display.display_clear
     @display.jaarix('Game Rules!!!!')
-    puts 'Press enter....'
+    puts 'Press enter to continue....'
     gets
     rules
     win_1
@@ -23,9 +23,8 @@ class Gamerules
 
   def rules
     @display.display_clear
-    puts 'Page 1 of 5'
     @display.jaarix('The game consist on 2 players switching turn and marking a place on a 3x3 board!')
-    puts 'Press enter con continue.....'
+    puts 'Press enter to continue.....'
     gets
   end
 
@@ -35,7 +34,7 @@ class Gamerules
     @display.jaarix('You can win by having a line of 3 O/X like below!')
     @display.board_print(['o', 'o', 'o', 4, 5, 6, 7, 8, 9])
     @display.board_print(['X', 2, 3, 'X', 5, 6, 'X', 8, 9])
-    puts 'Press enter con continue.....'
+    puts 'Press enter to continue.....'
     gets
   end
 
@@ -46,7 +45,7 @@ class Gamerules
     @display.board_print(['X', 'X', 'X', 4, 5, 6, 7, 8, 9])
     @display.board_print([1, 2, 3, 'O', 'O', 'O', 7, 8, 9])
     @display.board_print([1, 2, 3, 4, 5, 6, 'O', 'O', 'O'])
-    puts 'Press enter con continue.....'
+    puts 'Press enter to continue.....'
     gets
   end
 
@@ -57,17 +56,17 @@ class Gamerules
     @display.board_print(['O', 2, 3, 'O', 5, 6, 'O', 8, 9])
     @display.board_print([1, 'X', 3, 4, 'X', 6, 7, 'X', 9])
     @display.board_print([1, 2, 'O', 4, 5, 'O', 7, 8, 'O'])
-    puts 'Press enter con continue.....'
+    puts 'Press enter to continue.....'
     gets
   end
 
   def win_4
     @display.display_clear
     puts 'Page 4 of 5'
-    @display.jaarix('You can even win if manage do one diagonal!')
+    @display.jaarix('You can even win if you manage do one diagonal!')
     @display.board_print(['O', 2, 3, 4, 'O', 6, 7, 8, 'O'])
     @display.board_print([1, 2, 'X', 4, 'X', 6, 'X', 8, 9])
-    puts 'Press enter con continue.....'
+    puts 'Press enter to continue.....'
     gets
   end
 
@@ -76,7 +75,7 @@ class Gamerules
     puts 'Page 5 of 5'
     @display.jaarix('If no one can complete a line of 3 is a tie!')
     @display.board_print(%w[O X O O O X X X O])
-    puts 'Press enter con continue.....'
+    puts 'Press enter to continue to menu.....'
     gets
   end
 end
