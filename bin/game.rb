cf065b0100565f3160b86aa2c7c0cffee14dde35
errea <erreakezy@gmail.com>
2021-03-25 16:25:59 +0100

    add method initialize to the game module

--- bin/game.rb
+++ bin/game.rb
@@ -2,5 +2,26 @@ require_relative 'menu.rb'
 
 module Game
 
+    def initialize
+        @first_player = nil
+        @second_player = nil
+        @get_current_player = nil
+    end
  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#create  A WHILE TO CHANGE B/W player one and two
