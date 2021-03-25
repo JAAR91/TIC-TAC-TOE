e8ca3c4a3dc70822749e1945fdccff6bdc4db39f
errea <erreakezy@gmail.com>
2021-03-25 17:11:25 +0100

    add method game turn checker to check spot on the board to the game module

--- bin/game.rb
+++ bin/game.rb
@@ -12,7 +12,7 @@ module Play_Game
     end
 
     def take_turns(player_turn)
-        player_turn = Turn.ne
+        player_turn = turn.new
         take_turns.display_clear
         puts '+----------------------------------------------+'
         puts 'It is #{first_player} turn:'
@@ -23,6 +23,15 @@ module Play_Game
         second_player = second_player(gets.chomp)
         puts 'Please select any valid spot or cell on the board to play'
       end
+
+      def game_turn_checker()
+        game_turn_checker = false
+        while game_turn_checker == false
+          print "Please enter only numbers beetwen #{initial} and #{last} please!! :"
+          game_turn_checker = true if
+        end
+      end
+    
     
     end
  
